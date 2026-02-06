2a96f525ca7e7073cc637798da9d235de097615a
Kil1phil <PhilipWieseke@outlook.de>
2026-02-06 13:32:56 +0100

    Fixed Code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -16,8 +16,7 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	//splitAuth := strings.Fields(authHeader)
-        splitAuth := strings.Strings(authHeader," ")
+	splitAuth := strings.Fields(authHeader)
 
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
